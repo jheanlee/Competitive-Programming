24c0842acbebdb108bf783640a947184cbb7818f
jheanlee <136943417+jheanlee@users.noreply.github.com>
2024-04-20 20:16:45 +0800

    Create timer.cpp

---
+++ Tools/timer.cpp
@@ -0,0 +1,20 @@
+#include <time.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void run_with_timer() {
+  clock_t start_time, end_time;
+  start_time = clock();
+
+/*---Write code to time here---*/
+
+
+  
+/*-----------------------------*/
+  end_time = clock();
+  double time_taken = double(end_time - start_time) / double(CLOCKS_PER_SEC);
+    cout << "Execution Time: " << fixed 
+         << time_taken << setprecision(5);
+    cout << " sec" << endl;
+}
